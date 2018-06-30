2ba0b0e46d974c91e4f33a13c5633e184dc57c66
Paulo Abelha <pauloabelha@gmail.com>
2018-06-30 19:01:36 +0100

    Adding conda environments

--- conda_export.sh
+++ conda_export.sh
@@ -4,6 +4,7 @@
 FOLDER=$1
 
 NOW=$(date "+%Y-%m-%d")
+echo "Creating folder: "$FOLDER/envs-$NOW
 mkdir $FOLDER/envs-$NOW
 ENVS=$(conda env list | grep '^\w' | cut -d' ' -f1)
 echo "Environments: "$ENVS
@@ -11,5 +12,6 @@ for env in $ENVS; do
     echo "Exporting environment "$env
     source activate $env
     conda env export > $FOLDER/envs-$NOW/$env.yml
-    echo "Exporting $env"
+    echo "Done. Exported to: "$FOLDER/envs-$NOW/$env.yml
 done
+echo "Done exporting all environments"
